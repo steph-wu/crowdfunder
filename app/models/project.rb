8f5da39a96241a282e8eed39934df9bfa0b49621
EFFY MIN <effymin@gmail.com>
2017-02-22 16:28:29 -0500

    adding owner and user model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,6 @@ class Project < ActiveRecord::Base
   accepts_nested_attributes_for :rewards
   validates_presence_of :title, :description, :goal, :start_date, :end_date
 
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true, on: :create
 
   validates :email, uniqueness: true
+
+  has_many :pledges
+
 end
