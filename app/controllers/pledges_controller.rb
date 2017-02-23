0302d3850c52185cd1893b452161e9289fbe54bb
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-22 19:42:48 -0500

    Prevent pledges from being made to unavailable rewards

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -5,27 +5,17 @@ class PledgesController < ApplicationController
     @reward = Reward.find(params[:reward_id])
   end
 
-  def new
-    @pledge = Pledge.new
-  end
-
   def create
-
-    @pledges = @reward.pledges
 
     if @reward.available?
       @pledge = Pledge.new(pledge_params)
       @pledge.user = current_user
       @pledge.reward = Reward.find(params[:reward_id])
-
       if @pledge.save
         redirect_to project_url(@pledge.project), notice: "You have successfully backed #{@pledge.project.title}!"
       end
-
-    # else
-    #   @project =
-    #   render 'projects/show'
-    #   print "This didn't work dude"
+    else
+      redirect_to project_url, notice: "This reward is unavailable!"
     end
 
   end
