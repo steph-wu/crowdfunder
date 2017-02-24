78f427f4e455076c69459618ddcfef33118cf01d
EFFY MIN <effymin@gmail.com>
2017-02-24 15:58:35 -0500

    start comment js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,3 +1,17 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function() {
+  $("#new_comment").on('keypress', function(e){
+      if(e.keyCode==13){
+          $.ajax({
+              url: $(this).attr('action'),
+              type: $(this).attr('method'),
+              dataType: 'script',
+              data: $(this).serialize(),
+          });
+      }
+  });
+});
+});
