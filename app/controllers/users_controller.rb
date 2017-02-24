b46cfad4ce78ad24ad001d004c453419e634bd74
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-24 17:19:41 -0500

    Expand search function to category and and implement flexbox layout to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to projects_url
+      redirect_to root_path
     else
       render 'new'
     end
